0b741b47aea5eb58821bcee1782e4ea8e9494b00
brayansi <v.brayansantos@gmail.com>
2017-06-01 21:14:09 -0300

    tala cadastro

--- script/telaprincipal.js
+++ script/telaprincipal.js
@@ -1,24 +1,38 @@
 $(document).ready(function(){
     
     //obj Login
-    var Login={
+    var conta={
         contas: [],
-        adicionarConta: function(contato){
-        
+        adicionarConta: function(conta){
+                    
         }
+
+    
 
     }
     //Pegar as informaçães
 
 
     //Login
-    $('#frmCadastro').submit(function(event){
+    $('.frmLogin').submit(function(event){
 		event.preventDefault();
 		var contato = {
-			nome: $('#txtNome').val(),
-            senha: $('#txtSenha').val(),
+			nome: $('.txtNome').val(),
+            senha: $('.txtSenha').val(),
         }
         console.log(contato);
+    });
+    
+    //cadastros
+    $('.frmCadastro').submit(function(event){
+        event.preventDefault();
+        var conta = {
+            nome: $('.cadastroNome').val(),
+            email: $('.cadastroEmail').val(),
+            senha: $('.cadastroSenha').val(),
+            confSenha: $('.cadastroConfSenha').val(),
+        }
+        conta.adicionarConta(conta);
     });
 
     //comprar;
