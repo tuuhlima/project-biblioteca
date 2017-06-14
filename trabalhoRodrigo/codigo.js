955d89f74807451bef4b5c0a993ffb69a9cf0249
brayansi <v.brayansantos@gmail.com>
2017-06-14 11:45:39 -0300

    Cadastro Realizado

--- trabalhoRodrigo/codigo.js
+++ trabalhoRodrigo/codigo.js
@@ -1,15 +1,22 @@
-start();
-btn();
 
-function start(){
-    var agenda={
+(function(){
+       var agenda={
         contatos: [],
-        
         adicionar: function(contato){
-
-            
-            this.contatos.push(contato);
-            salvar();
+            this.pegarContatosSalvos();
+            if(contato){
+                for (var indice in this.contatos) {
+                    if(contato.nome === this.contatos[indice].nome){
+                        throw new Error('O contato ja existe !!!');
+                        return;
+                    }
+                }
+                this.contatos.push(contato);
+                alert('Cadastro Realizado com Sucesso');
+                this.salvar();
+            }else{
+                throw new Error('Erro ao Realizar Cadastro');
+            }
         },
 
 
@@ -19,10 +26,27 @@ function start(){
 		},
 		pegarContatosSalvos: function () {
 			this.contatos = JSON.parse(localStorage.contatos);
-		}     
+		},     
     }
-}
-
+     
+    var btn = document.getElementById('myform');
+    btn.onsubmit = function mysubmit(event){
+        event.preventDefault();
+         var contato = {
+            nome: document.getElementById('nome').value,
+            email: document.getElementById('email').value,
+            telefone: document.getElementById('telefone').value,
+        } 
+        try{
+            agenda.adicionar(contato);
+        }catch(e){
+            alert(e.message);
+        }      
+    }
+    
+    
+    
+})();
 
 function btn(){
     var btn = document.getElementById('bntCadastrar');
@@ -31,10 +55,7 @@ function btn(){
         var email = document.getElementById('email');
         var telefone = document.getElementById('telefone');
 
-        var contato = {
-            nome: this.nome.value(),
-        }
-
+       
         
         agenda.adicionar(contato);
     };
