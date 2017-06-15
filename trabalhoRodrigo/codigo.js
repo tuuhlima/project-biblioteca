3b5d39253167b9a6b966f66b8d3f641a8c057697
Brayansi <v.brayansantos@gmail.com>
2017-06-14 21:27:25 -0300

    OK

--- trabalhoRodrigo/codigo.js
+++ trabalhoRodrigo/codigo.js
@@ -1,69 +1,81 @@
 
-(function(){
-       var agenda={
+(function () {
+    var agenda = {
         contatos: [],
-        adicionar: function(contato){
-            this.pegarContatosSalvos();
-            if(contato){
-                for (var indice in this.contatos) {
-                    if(contato.nome === this.contatos[indice].nome){
-                        throw new Error('O contato ja existe !!!');
-                        return;
-                    }
-                }
+        adicionar: function (contato) {
+            if ("undefined" === typeof variable) {
                 this.contatos.push(contato);
                 this.salvar();
                 alert('Cadastro Realizado com Sucesso');
-            }else{
-                throw new Error('Erro ao Realizar Cadastro');
+            } else {
+                this.pegarContatosSalvos();
+                if (contato) {
+                    for (var indice in this.contatos) {
+                        if (contato.nome === this.contatos[indice].nome) {
+                            throw new Error('O contato ja existe !!!');
+                            return;
+                        }
+                    }
+                    this.contatos.push(contato);
+                    this.salvar();
+                    alert('Cadastro Realizado com Sucesso');
+                } else {
+                    throw new Error('Erro ao Realizar Cadastro');
+                }
             }
-            for (var indice in this.contatos) {
-                console.log(this.contatos[indice]);
-            }
+        this.listar();
         },
 
         listar: function () {
-			this.pegarContatosSalvos();
-			for (indice in this.contatos) {
-				var contato = this.contatos[indice];
-				criarNovoContato(contato);
-			}
-		},
+            this.pegarContatosSalvos();
+            for (indice in this.contatos) {
+                var contato = this.contatos[indice];
+                criarNovoContato(contato);
+            }
+        },
         salvar: function () {
-			var contatosString = JSON.stringify(this.contatos);
-			localStorage.contatos = contatosString;
-		},
-		pegarContatosSalvos: function () {
-			this.contatos = JSON.parse(localStorage.contatos);
-		},     
+            var contatosString = JSON.stringify(this.contatos);
+            localStorage.contatos = contatosString;
+        },
+        pegarContatosSalvos: function () {
+            this.contatos = JSON.parse(localStorage.contatos);
+        },
     }
-     
-    document.getElementById('myform').onsubmit = function pegarDados(event){
+
+    document.getElementById('myform').onsubmit = function pegarDados(event) {
         event.preventDefault();
-         var contato = {
+        var contato = {
             nome: document.getElementById('nome').value,
             email: document.getElementById('email').value,
             telefone: document.getElementById('telefone').value,
-        } 
-        try{
+        }
+        try {
             agenda.adicionar(contato);
-        }catch(e){
+        } catch (e) {
             alert(e.message);
-        }      
+        }
     }
 
-    var criarNovoContato = function (contato){
-        var listTable = document.getElementById('listTable');
-        var table = document.createElement('table');
+    var criarNovoContato = function (contato) {
+
         var tr = document.createElement('tr');
-        var thnome = document.createElement('th');
-        var themail = document.createElement('th');
         var tdnome = document.createElement('td');
-        var tdnome = document.createElement('td');
-        
-        console.log(contato.nome);
+        var tdemail = document.createElement('td');
+        var tdtelefone = document.createElement('td');
+        var listTable = document.getElementById('listTable');
+        var nome = document.createTextNode(contato.nome);
+        var email = document.createTextNode(contato.email);
+        var telefone = document.createTextNode(contato.telefone);
+
+        listTable.appendChild(tr);
+        tr.appendChild(tdnome);
+        tr.appendChild(tdemail);
+        tr.appendChild(tdtelefone);
+        tdnome.appendChild(nome);
+        tdemail.appendChild(email);
+        tdtelefone.appendChild(telefone);
 
     }
-    agenda.listar(); 
+    agenda.listar();
 })();
 
